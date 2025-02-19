b0463249ad1a7471c9dd0e7b83d49c1b7e89d9b3
Piotr Poterek <peterpoterek0@gmail.com>
2025-02-19 03:44:04 +0100

    transition between stages

--- components/quiz/Quiz.tsx
+++ components/quiz/Quiz.tsx
@@ -3,6 +3,7 @@
 import hiragana from "@/data/hiragana.json";
 import { useState, useEffect } from "react";
 import useQuizStore from "@/store/quizStore";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface HiraganaChar {
   kana: string;
@@ -13,6 +14,7 @@ const Quiz = () => {
   const [randomChar, setRandomChar] = useState<HiraganaChar | null>(null);
   const [choices, setChoices] = useState<string[]>([]);
   const [prevChar, setPrevChar] = useState<HiraganaChar | null>(null);
+  const [transitioning, setTransitioning] = useState(false);
 
   const {
     progress,
@@ -72,14 +74,20 @@ const Quiz = () => {
     if (isCorrect) {
       if (progress + 1 === maxProgress) {
         incrementProgress(1);
-        if (mode === "romaji-to-kata") {
-          alert("Next stage ✔");
-          resetProgress();
-          switchMode("kata-to-romaji");
-          generateQuestion();
-        } else if (mode === "kata-to-romaji") {
-          switchMode("completed");
-        }
+
+        setTimeout(() => {
+          if (mode === "romaji-to-kata") {
+            setTransitioning(true);
+            setTimeout(() => {
+              resetProgress();
+              switchMode("kata-to-romaji");
+              setTransitioning(false);
+              generateQuestion();
+            }, 1000);
+          } else if (mode === "kata-to-romaji") {
+            switchMode("completed");
+          }
+        }, 100); // delay to sure progress updates
       } else {
         incrementProgress(1);
         generateQuestion();
@@ -129,27 +137,47 @@ const Quiz = () => {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center gap-4">
-        <p className="text-7xl">
-          {mode === "romaji-to-kata" ? randomChar?.romaji : randomChar?.kana}
-        </p>
-        <div className="grid grid-cols-5 gap-2">
-          {choices.map((char, index) => (
-            <button
-              onClick={() => checkAnswer(char)}
-              className="w-[100px] h-[100px] text-4xl flex items-center justify-center border border-gray-500 rounded relative"
-              key={index}
-            >
-              {char}
-              <span className="absolute top-1 left-1 text-sm text-gray-500">
-                {index + 1}
-              </span>
-            </button>
-          ))}
-        </div>
-      </div>
-    </div>
+    <AnimatePresence mode="wait">
+      {transitioning ? (
+        <motion.div
+          key="transition"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="text-4xl font-bold text-center"
+        >
+          Next Stage ✔
+        </motion.div>
+      ) : (
+        <motion.div
+          key="quiz"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="flex flex-col items-center gap-4"
+        >
+          <p className="text-7xl">
+            {mode === "romaji-to-kata" ? randomChar?.romaji : randomChar?.kana}
+          </p>
+          <div className="grid grid-cols-5 gap-2">
+            {choices.map((char, index) => (
+              <button
+                onClick={() => checkAnswer(char)}
+                className="w-[100px] h-[100px] text-4xl flex items-center justify-center border border-gray-500 rounded relative"
+                key={index}
+              >
+                {char}
+                <span className="absolute top-1 left-1 text-sm text-gray-500">
+                  {index + 1}
+                </span>
+              </button>
+            ))}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
